73c77343450b4a7d64f3aaf59ce5fe0ca055619f
Marian Feiler <mf@urbanstudio.de>
2021-08-17 11:30:10 +0200

    Update api.php
    added array $radissoCallbacks with short description

--- Wrapper/api.php
+++ Wrapper/api.php
@@ -11,6 +11,18 @@ require(dirname(__FILE__)."/classes/Radisso/Wrapper.php");
  * Get the POST Body with:
  */
 $inputData = file_get_contents('php://input');
+/**
+ * Define some Callbacks for Incoming requests.
+ * Key is the request type
+ * Value = String for functions, array for static methods in a class
+ * All your callback methods will receive stdClass $data
+ */
+$radissoCallbacks = [
+    "userDataPush"          => ["\Radisso\Client", "userDataPush"],
+    "createUserSession"     => ["\Radisso\Client", "createUserSession"],
+    "requestUserListPush"   => ["\Radisso\Client", "requestUserListPush"],
+    "pwPush"                => ["\Radisso\Client", "pwPush"]
+];
 
 $radisso = new \Radisso\Wrapper;
 // echo the result is sending the encoded response
